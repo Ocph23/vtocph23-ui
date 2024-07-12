48113c813e62bef4b6612eedda750c54a0f55e2b
ocph23 <ocph23@gmail.com>
2024-07-12 12:17:37 +0900

    test

--- src/components/VTCard.vue
+++ src/components/VTCard.vue
@@ -1,14 +1,13 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <div class="bg-white rounded-lg border shadow-md md:flex-row  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 md:max-w-full mb-4">
-        <div
-            class="p-5 ">
+    <div
+        class="bg-white rounded-lg border shadow-md md:flex-row  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 md:max-w-full mb-4">
+        <div class="p-5 ">
             <div class="mb-8 md:flex md:flex-row justify-between">
                 <h1 v-if="title" class=" uppercase text-xl font-semibold">{{ title }}</h1>
                 <slot name="right-side" />
             </div>
             <slot />
-
         </div>
         <hr v-if="$slots.footerSide" class="border-gray-500 border-t-[.9px]">
         <div class="flex justify-center py-3">
