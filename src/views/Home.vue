148386c7d4b87078db02d5c1416d80733b656217
ocph23 <ocph23@gmail.com>
2024-07-16 13:14:18 +0900

    fix some

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,18 @@
 <template>
     <VTCard class="h-screen" title="Title" subtitle="subtitle" description="description">
-        <VTSelect></VTSelect>
+        <VTAutocomplete v-model="selected" :sources="data"></VTAutocomplete>
     </VTCard>
 </template>
 
 <script setup lang="ts">
+import VTAutocomplete from '@/components/VTAutocomplete.vue';
 import VTCard from '@/components/VTCard.vue';
 import VTSelect from '@/components/VTSelect/VTSelect.vue';
+import { ref } from 'vue';
+
+
+const selected = ref(null)
+const data = [{ name: "Yoseph Kungkung", value: 1 }, { name: "Yoseph X", value: 2 }]
+
+
 </script>
