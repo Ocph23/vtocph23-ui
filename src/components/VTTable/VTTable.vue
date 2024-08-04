cbd255021da230c8e69a1c89becaff94a047ebf4
victorown <abraham2001ohoiwutun@gmail.com>
2024-08-04 15:08:03 +0900

    tse

--- src/components/VTTable/VTTable.vue
+++ src/components/VTTable/VTTable.vue
@@ -79,7 +79,7 @@
 
         </tbody>
         <tfoot v-if="$slots.footer && isLargeScreen" class="bg-slate-200 dark:bg-slate-700">
-          <slot name="footer" v-bind="sortedData"></slot>
+          <slot name="footer"></slot>
         </tfoot>
       </table>
     </div>
