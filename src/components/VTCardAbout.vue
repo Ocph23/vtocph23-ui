e861eb737f151bc131c1c84a64e5210a0de90982
victorown <abraham2001ohoiwutun@gmail.com>
2024-08-05 18:33:26 +0900

    update

--- src/components/VTCardAbout.vue
+++ src/components/VTCardAbout.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="card-container">
-        <img :src="src" alt="Profile Picture">
+        <div class="rounded-full overflow-hidden flex justify-center">
+            <img :src="src" alt="Profile Picture">
+        </div>
         <div class="card-body">
             <div class="card-name">{{ name }}</div>
             <p class="card-title">{{ title }}</p>
@@ -25,32 +27,36 @@ const props = defineProps<Props>()
 </script>
 
 <style scoped>
-img {
-    @apply w-full h-60 object-cover bg-gray-400;
+img{
+    @apply w-40 rounded-full h-40 object-cover bg-gray-400;
     object-position: top;
 }
 
-.card-container {
-    @apply max-w-sm rounded overflow-hidden bg-transparent transition-all ease-in-out duration-300 delay-75 hover:shadow-lg hover:scale-105
+.card-container{
+    @apply max-w-sm rounded overflow-hidden 
+    bg-transparent transition-all 
+    ease-in-out duration-300 delay-75 
+    hover:shadow-lg hover:scale-105
 }
 
-.card-body {
-    @apply px-3 py-4
+.card-body{
+    @apply px-3 py-4 flex flex-col justify-center items-center
 }
 
-.card-name {
+.card-name{
     @apply font-bold text-xl mb-2
 }
 
-.card-title {
+.card-title{
     @apply text-gray-700 dark:text-gray-300 text-start text-base
 }
 
-.card-footer {
+.card-footer{
     @apply px-3 text-start pb-2
 }
 
-.card-desc {
+.card-desc{
     @apply text-gray-600 dark:text-gray-400 text-sm
 }
+
 </style>
