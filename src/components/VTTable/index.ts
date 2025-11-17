e538cb42ff3585b9bec9e2794585fc801b3989fd
ocph23 <ocph23@gmail.com>
2025-11-17 23:20:44 +0900

    fix VTTableSource

--- src/components/VTTable/index.ts
+++ src/components/VTTable/index.ts
@@ -1,36 +1,31 @@
-
 export interface VTTableColumn {
-    propName?: string | null;
-    title: string,
-    type?: ColumnType,
-    name?: string,
-    headerClass?: string,
-    headerPosition?: 'left' | 'center' | 'right' | 'between',
-    rowClass?: string,
-    hiddenColumn?: Boolean,
-    checked?: Boolean,
-    isMobileHeader?: Boolean
+  propName?: string | null
+  title: string
+  type?: ColumnType
+  name?: string
+  headerClass?: string
+  headerPosition?: 'left' | 'center' | 'right' | 'between'
+  rowClass?: string
+  hiddenColumn?: Boolean
+  checked?: Boolean
+  isMobileHeader?: Boolean
 }
 
-
-
-export interface VTTableSource {
-    data: any[],
-    paginate?: VTTablePagination
-    totalRecords: number
+export interface VTTableSource<T> {
+  data: T[]
+  paginate?: VTTablePagination
+  totalRecords: number
 }
-
-
 
 export interface VTTablePagination {
-    currentPage: number
-    pageSize: number
-    searchTerm?: string
-    sortOrder: 'asc' | 'desc' | null
-    columnOrder?: string
+  currentPage: number
+  pageSize: number
+  searchTerm?: string
+  sortOrder: 'asc' | 'desc' | null
+  columnOrder?: string
 }
 
-export type ColumnType = "Text" | "Number" | "Boolean" | "Tanggal" | "Custome";
+export type ColumnType = 'Text' | 'Number' | 'Boolean' | 'Tanggal' | 'Custome'
 
-export type MethodType = "Post" | "Get" | "Delete" | "Put" | "Default";
-export type MethodXType = "All" | "Paginate";
+export type MethodType = 'Post' | 'Get' | 'Delete' | 'Put' | 'Default'
+export type MethodXType = 'All' | 'Paginate'
